997dcf60126abef654a2405e9741d8486c45046c
Gonzalo Techeira <gnxa95@hotmail.com>
2018-10-01 21:02:15 -0300

    ...

---
+++ FigurasEntidades/Figuras.vb
@@ -0,0 +1,21 @@
+﻿Public Class Figuras
+    Private _nombre As String
+    Private _calculararea As Single
+
+    Public Sub New(nombre As String)
+        Me.Nombre = nombre
+    End Sub
+
+    Public Property Nombre As String
+        Get
+            Return _nombre
+        End Get
+        Set(value As String)
+            _nombre = value
+        End Set
+    End Property
+    Public Overridable Function CalcularArea() As Single
+        Return _calculararea
+    End Function
+End Class
+
---
+++ FigurasEntidades/Triangulo.vb
@@ -0,0 +1,30 @@
+﻿Public Class Triangulo
+    Inherits Figuras
+
+    Private _base As UShort
+    Private _altura As UShort
+
+    Public Sub New(nombre As String)
+        MyBase.New(nombre)
+    End Sub
+
+    Public Property Base As UShort
+        Get
+            Return _base
+        End Get
+        Set(value As UShort)
+            _base = value
+        End Set
+    End Property
+    Public Property Altura As UShort
+        Get
+            Return _altura
+        End Get
+        Set(value As UShort)
+            _altura = value
+        End Set
+    End Property
+    Public Overrides Function CalcularArea() As Single
+        Return Base * Altura / 2
+    End Function
+End Class
